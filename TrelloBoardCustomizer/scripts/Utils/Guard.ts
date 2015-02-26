e1488586823e871461ce83f82b4cf37161dab67b
Yura95st <yura95st@gmail.com>
2015-02-26 22:24:35 +0200

    Add Guard utility class.

---
+++ TrelloBoardCustomizer/scripts/Utils/Guard.ts
@@ -0,0 +1,18 @@
+﻿module Utils
+{
+    export class Guard
+    {
+        static notNull(argument: any, argumentName: string): void
+        {
+            if (argument === undefined)
+            {
+                throw new RangeError("Argument '" + argumentName + "' is undefined");
+            }
+
+            if (argument === null)
+            {
+                throw new RangeError("Argument '" + argumentName + "' is null");
+            }
+        }
+    }
+}
