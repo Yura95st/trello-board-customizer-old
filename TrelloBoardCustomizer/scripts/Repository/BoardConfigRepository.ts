be67b363b00d33a3ca6ab9309683d59b9960b6b3
Yura95st <yura95st@gmail.com>
2015-02-27 22:47:46 +0200

    Add IBoardConfigRepository for storing configs for different boards.

---
+++ TrelloBoardCustomizer/scripts/Repository/BoardConfigRepository.ts
@@ -0,0 +1,44 @@
+﻿/// <reference path="IBoardConfigRepository.ts" />
+/// <reference path="../DataStorage/IDataStorage.ts" />
+module Repository
+{
+    import IDataStorage = DataStorage.IDataStorage;
+    import Guard = Utils.Guard;
+
+    export class BoardConfigRepository implements IBoardConfigRepository
+    {
+        private _dataStorage: IDataStorage;
+
+        constructor(dataStorage: IDataStorage)
+        {
+            Guard.notNull(dataStorage, "dataStorage");
+
+            this._dataStorage = dataStorage;
+        }
+
+        insert(boardConfig: Models.BoardConfig): void
+        {
+        }
+
+        getById(boardConfigId: number): Models.BoardConfig
+        {
+            throw new Error("Not implemented");
+        }
+
+        load(): void
+        {
+        }
+
+        remove(boardConfig: Models.BoardConfig): void
+        {
+        }
+
+        save(): void
+        {
+        }
+
+        update(boardConfig: Models.BoardConfig): void
+        {
+        }
+    }
+}
---
+++ TrelloBoardCustomizer/scripts/Repository/IBoardConfigRepository.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="../Models/BoardConfig.ts" />
+
+module Repository
+{
+    import BoardConfig = Models.BoardConfig;
+
+    export interface IBoardConfigRepository
+    {
+        insert(boardConfig: BoardConfig): void;
+
+        getById(boardConfigId: number): BoardConfig;
+
+        load(): void;
+
+        remove(boardConfig: BoardConfig): void;
+
+        save(): void;
+
+        update(boardConfig: BoardConfig): void;
+    }
+} 
