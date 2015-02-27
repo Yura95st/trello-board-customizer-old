42e8b3551944d4aeaf510ee196f08b7547bb26a6
Yura95st <yura95st@gmail.com>
2015-02-27 22:21:40 +0200

    Add notNullOrEmpty method to the Guard utility class.

--- TrelloBoardCustomizer/scripts/Utils/Guard.ts
+++ TrelloBoardCustomizer/scripts/Utils/Guard.ts
@@ -6,12 +6,22 @@
         {
             if (argument === undefined)
             {
-                throw new RangeError("Argument '" + argumentName + "' is undefined");
+                throw new RangeError("Argument '" + argumentName + "' can't be undefined");
             }
 
             if (argument === null)
             {
-                throw new RangeError("Argument '" + argumentName + "' is null");
+                throw new RangeError("Argument '" + argumentName + "' can't be null");
+            }
+        }
+
+        static notNullOrEmpty(argument: any, argumentName: string): void
+        {
+            Guard.notNull(argument, argumentName);
+
+            if (argument.length === 0)
+            {
+                throw new RangeError("Argument '" + argumentName + "' can't be empty");
             }
         }
     }
