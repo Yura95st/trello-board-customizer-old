e26cce32f6a908eae54132b67d5b842023218dbe
Yura95st <yura95st@gmail.com>
2015-02-27 22:44:43 +0200

    Add IDataStorage interface.

---
+++ TrelloBoardCustomizer/scripts/DataStorage/ChromeDataStorage.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="IDataStorage.ts" />
+
+module DataStorage
+{
+    export class ChromeDataStorage implements IDataStorage
+    {
+        getItem(key: string): string
+        {
+            throw new Error("Not implemented");
+        }
+
+        setItem(key: string, value: string): void
+        {
+            throw new Error("Not implemented");
+        }
+    }
+} 
---
+++ TrelloBoardCustomizer/scripts/DataStorage/IDataStorage.ts
@@ -0,0 +1,9 @@
+﻿module DataStorage
+{
+    export interface IDataStorage
+    {
+        getItem(key: string): string;
+
+        setItem(key: string, value: string): void;
+    }
+} 
