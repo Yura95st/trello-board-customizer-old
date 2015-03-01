8209e2a3395df509622046de4647e221d4e7c240
Yura95st <yura95st@gmail.com>
2015-03-01 14:07:18 +0200

    Modify LocalDataStorage: check key value for Null or Empty.

--- TrelloBoardCustomizer/scripts/DataStorage/LocalDataStorage.ts
+++ TrelloBoardCustomizer/scripts/DataStorage/LocalDataStorage.ts
@@ -1,16 +1,23 @@
 ﻿/// <reference path="IDataStorage.ts" />
+/// <reference path="../Utils/Guard.ts" />
 
 module DataStorage
 {
+    import Guard = Utils.Guard;
+
     export class LocalDataStorage implements IDataStorage
     {
         getItem(key: string): string
         {
+            Guard.notNullOrEmpty(key, "key");
+
             return localStorage[key];
         }
 
         setItem(key: string, value: string): void
         {
+            Guard.notNullOrEmpty(key, "key");
+
             localStorage[key] = value;
         }
     }
