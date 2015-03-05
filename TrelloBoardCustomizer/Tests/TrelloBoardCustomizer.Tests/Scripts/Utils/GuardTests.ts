737787c3e3f521fb92e6e0608f5c8e499991a5ae
Yura95st <yura95st@gmail.com>
2015-03-05 02:02:48 +0200

    Add GuardTests to the Test project.

---
+++ TrelloBoardCustomizer/Tests/TrelloBoardCustomizer.Tests/Scripts/Utils/GuardTests.ts
@@ -0,0 +1,82 @@
+﻿/// <reference path="../References.ts" />
+module TrelloBoardCustomizer.Tests.Utils
+{
+    import Guard = TrelloBoardCustomizer.Utils.Guard;
+
+    QUnit.module("GuardTests");
+
+    QUnit.test("notNull_ArgumentIsNull_ThrowsRangeError",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = null;
+
+        assert.throws(() =>
+        {
+            Guard.notNull(argumentValue, argumentName);
+        }, RangeError);
+    });
+
+    QUnit.test("notNull_ArgumentIsNotNull_DoNothing",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = Object;
+
+        Guard.notNull(argumentValue, argumentName);
+
+        assert.ok(true);
+    });
+
+    QUnit.test("notNull_ArgumentIsUndefined_ThrowsRangeError",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = undefined;
+
+        assert.throws(() =>
+        {
+            Guard.notNull(argumentValue, argumentName);
+        }, RangeError);
+    });
+
+    QUnit.test("notNullOrEmpty_ArgumentIsUndefined_ThrowsRangeError",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = undefined;
+
+        assert.throws(() =>
+        {
+            Guard.notNullOrEmpty(argumentValue, argumentName);
+        }, RangeError);
+    });
+
+    QUnit.test("notNullOrEmpty_ArgumentIsNull_ThrowsRangeError",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = null;
+
+        assert.throws(() =>
+        {
+            Guard.notNullOrEmpty(argumentValue, argumentName);
+        }, RangeError);
+    });
+
+    QUnit.test("notNullOrEmpty_ArgumentIsEmpty_ThrowsRangeError",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = "";
+
+        assert.throws(() =>
+        {
+            Guard.notNullOrEmpty(argumentValue, argumentName);
+        }, RangeError);
+    });
+
+    QUnit.test("notNullOrEmpty_ArgumentIsNotEmpty_DoNothing",(assert: QUnitAssert) =>
+    {
+        var argumentName: string = "argumentName";
+        var argumentValue: any = "argumentValue";
+
+        Guard.notNullOrEmpty(argumentValue, argumentName);
+
+        assert.ok(true);
+    });
+}
